54be5537fc1874d1ee0199bfde568a19c8285b14
Mofisam <malaosamuel2020@gmail.com>
2026-04-25 16:45:04 +0100

    CSRF token to page via meta tag

--- includes/header.php
+++ includes/header.php
@@ -17,6 +17,10 @@ $is_logged_in = isset($_SESSION['user_id']);
 $user_role = $_SESSION['user_role'] ?? null;
 $user_name = $_SESSION['user_name'] ?? null;
 ?>
+<?PHP if (empty($_SESSION['csrf_token'])) {
+    $_SESSION['csrf_token'] = bin2hex(random_bytes(32));
+}
+?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -29,9 +33,11 @@ $user_name = $_SESSION['user_name'] ?? null;
     <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
     <link rel="shortcut icon" href="/favicon.ico" />
     <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-    <meta name="apple-mobile-web-app-title" content="Clinical Physiology Academy" />
+    <meta name="apple-mobile-web-app-title" content="Green Agric" />
     <link rel="manifest" href="/site.webmanifest" />
     
+    <meta name="csrf-token" content="<?php echo $_SESSION['csrf_token']; ?>">
+
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     
